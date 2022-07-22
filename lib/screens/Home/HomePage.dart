e9100e05975ad3db14f8526e08c706040684a1bd
TIENTCHEUisrael <tientcheuisrael@gmail.com>
2022-07-22 18:32:34 +0200

    good

--- lib/screens/Home/HomePage.dart
+++ lib/screens/Home/HomePage.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../../models/user/user.dart';
+import './CommandesPage.dart';
+import './NotificationPage.dart';
+import './ChatsPage.dart';
+import './ProfilPage.dart';
 
 class Home extends StatefulWidget {
-  final User user;
+  final User person;
 
-  Home({required this.user});
+  Home({required this.person});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  String _texte = 'Home';
+  int currentTab = 3;
+  final List<Widget> screens = [
+    CommandesPage(),
+    ChatsPage(),
+    NotificationPage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/login/loginPage.dart
+++ lib/screens/login/loginPage.dart
@@ -163,7 +163,7 @@ class _LoginPageState extends State<LoginPage> {
                           return Navigator.of(context).pushReplacement(
                             MaterialPageRoute(builder: (ctx) {
                               return Home(
-                                user: result,
+                                person: result,
                               );
                             }),
                           );
